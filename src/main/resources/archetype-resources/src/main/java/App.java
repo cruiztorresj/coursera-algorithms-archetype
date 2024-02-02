756c93470e9a5911e48a533afbb7561fa6f9cfec
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2024-02-02 16:55:00 -0600

    main: Archetype source

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
